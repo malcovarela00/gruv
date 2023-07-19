ede4fdf8e143c0183af3da34bf3de202f094ad87
malco <malco@biorem.cat>
2023-07-19 17:56:41 +0200

    correccion4

--- gruv/settings.py
+++ gruv/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'gruv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gruv',
+        'USER': 'malcovarela90',
+        'PASSWORD': 'temporal2023',
+        'HOST': 'malcovarela90.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
