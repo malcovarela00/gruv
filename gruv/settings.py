0bb5b66f9d539119e5362a378fcc5362dc9b6c33
malco <malco@biorem.cat>
2023-07-21 10:54:40 +0200

    base de datos sqlite

--- gruv/settings.py
+++ gruv/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'gruv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'malcovarela90$default',
-        'USER': 'malcovarela90',
-        'PASSWORD': 'temporal2023',
-        'HOST': 'malcovarela90.mysql.pythonanywhere-services.com',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
