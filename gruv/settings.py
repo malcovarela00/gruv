d1b2113bba247f1ca2b57854898145308e5ee835
malco <malco@biorem.cat>
2023-08-08 12:39:40 +0200

    segunda prueba de BBDD mySQL

--- gruv/settings.py
+++ gruv/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'gruv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'malcovarela90$gruv_sql',
+        'USER': 'malcovarela90',
+        'PASSWORD': 'rHBKPbxgDZC3',
+        'HOST': 'malcovarela90.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
