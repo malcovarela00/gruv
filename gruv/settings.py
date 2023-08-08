6c9d8e0bc1f69825c5c9e84e8a80386385fd23c6
malco <malco@biorem.cat>
2023-08-08 17:54:26 +0200

    cambios en el admin/home

--- gruv/settings.py
+++ gruv/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'gruv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gruv_psql',
-        'USER': 'postgres',
-        'PASSWORD': 'malco',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
