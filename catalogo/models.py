e77ffc1d564af2c025c6f7789bb4e12ef4572bb3
malco <malco@biorem.cat>
2023-10-19 17:58:39 +0200

    sacado los 0

--- catalogo/models.py
+++ catalogo/models.py
@@ -27,7 +27,7 @@ class Pais(models.Model):
 
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=100)
-    comision = models.DecimalField(max_digits=4, decimal_places=2, default=1.00, verbose_name='Comision (%)')
+    comision = models.DecimalField(max_digits=4, decimal_places=2, default=1, verbose_name='Comision (%)')
     pais = models.ForeignKey('Pais', on_delete=models.CASCADE)
     update = models.DateTimeField(auto_now=True)
 
@@ -67,9 +67,9 @@ class Viaje(models.Model):
     fecha_vuelta = models.DateField()
 
     vendedor = models.ForeignKey('Vendedor', on_delete=models.CASCADE)
-    comision_vendedor = models.DecimalField(max_digits=4, decimal_places=2, default=0.00, validators=[MinValueValidator(0)], verbose_name='Comision Vendedor (%)')
+    comision_vendedor = models.DecimalField(max_digits=4, decimal_places=2, default=0, validators=[MinValueValidator(0)], verbose_name='Comision Vendedor (%)')
 
-    pago_cliente_monto = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    pago_cliente_monto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     pago_cliente_estado = models.CharField(max_length=13, choices=OPCIONES_DE_PAGO)
     pago_cliente_moneda = models.CharField(max_length=10, choices=OPCIONES_DE_MODEDA, editable=False)
     pago_cliente_fecha_vencimiento = models.DateField()
@@ -79,12 +79,12 @@ class Viaje(models.Model):
     pago_proveedor_moneda = models.CharField(max_length=10, choices=OPCIONES_DE_MODEDA, editable=False)
     pago_proveedor_fecha_vencimiento = models.DateField(verbose_name='Fecha de Entrada en Gastos')
 
-    pago_proveedor = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    pago_proveedor = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     
-    ganancia_bruto = models.DecimalField(max_digits=10, decimal_places=2, default=0.00,)
-    ganancia_usd_vendedor = models.DecimalField(max_digits=10, decimal_places=2, default=0.00,)
-    ganancia_gruv = models.DecimalField(max_digits=10, decimal_places=2, default=0.00,)
-    ganancia_neta_porc = models.DecimalField(max_digits=10, decimal_places=2, default=0.00,)
+    ganancia_bruto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    ganancia_usd_vendedor = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    ganancia_gruv = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    ganancia_neta_porc = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     fecha_creacion = models.DateField() #default=timezone.now, editable=False
     update = models.DateTimeField(auto_now=True)
@@ -136,9 +136,9 @@ class Viaje(models.Model):
 
 class Transferencia(models.Model):
     salida = models.CharField(max_length=13, choices=OPCIONES_DE_TRANSFERENCIA)
-    salida_monto = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    salida_monto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     entrada = models.CharField(max_length=13, choices=OPCIONES_DE_TRANSFERENCIA)
-    entrada_monto = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    entrada_monto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     observacion = models.TextField(blank=True,  null=True, verbose_name='Observación')
     
     fecha_creacion = models.DateField() #default=timezone.now, editable=False
@@ -198,7 +198,7 @@ class Transferencia(models.Model):
 class PagoProveedor(models.Model):
     pais = models.ForeignKey('Pais', on_delete=models.CASCADE)
     tipo_pago = models.CharField(max_length=13, choices=OPCIONES_TIPO_PAGO_PROVEEDOR, verbose_name='Tipo de Pago')
-    monto = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    monto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     observacion = models.TextField(blank=True,  null=True, verbose_name='Observación')
 
     fecha_creacion = models.DateField() #default=timezone.now, editable=False
@@ -239,9 +239,9 @@ class PagoProveedor(models.Model):
 
 class Plan(models.Model):
     cliente = models.CharField(max_length=100)
-    monto_financiado = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name='Monto a Financiar')
+    monto_financiado = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Monto a Financiar')
     cantidad_cuotas = models.PositiveSmallIntegerField(verbose_name='Cantidad de Cuotas')
-    monto_por_cuota = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name='Monto por Cuota')
+    monto_por_cuota = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Monto por Cuota')
 
     fecha_creacion = models.DateTimeField(default=timezone.now, editable=False)
     update = models.DateTimeField(auto_now=True)
@@ -258,9 +258,9 @@ class Plan(models.Model):
 class Cuota(models.Model):
     plan = models.ForeignKey('Plan', on_delete=models.CASCADE)
     tipo_cuota = models.CharField(max_length=13, choices=OPCIONES_DE_TIPO_CUOTA)
-    monto = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    monto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     numero_cuota = models.PositiveSmallIntegerField(verbose_name='Número de Cuota')
-    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     pagado = models.BooleanField(default=False)
     fecha_vencimiento = models.DateField(verbose_name='Fecha de Vencimiento')
 
